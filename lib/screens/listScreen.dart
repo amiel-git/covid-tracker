85ea0a47993913c65d59bf8bdc6ecf4cdc9f674a
Amiel <afurongamielchrysdin@gmail.com>
2020-11-22 23:04:36 +0800

    Completed the search functionality for the listView

--- lib/screens/listScreen.dart
+++ lib/screens/listScreen.dart
@@ -1,5 +1,6 @@
 import 'package:covid_tracker/classes/country.dart';
 import 'package:covid_tracker/utilities/reusableCard.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:covid_tracker/utilities/constants.dart';
 import 'package:covid_tracker/utilities/reusableCard.dart';
@@ -12,12 +13,13 @@ class ListScreen extends StatefulWidget {
 
   var covidData = [];
 
+
   @override
   _ListScreenState createState() => _ListScreenState();
 }
 
 class _ListScreenState extends State<ListScreen> {
-
+  var _covidDataForDisplay = List<dynamic>();
   @override
   void initState()  {
     updateCovidData();
@@ -27,8 +29,10 @@ class _ListScreenState extends State<ListScreen> {
   Future<void> updateCovidData() async {
     EasyLoading.show(status: 'loading...');
     await NetworkHelper().getAllSummaryData().then((value) {
-      widget.covidData.addAll(value);
+       widget.covidData.addAll(value);
+
     });
+    _covidDataForDisplay = widget.covidData;
     setState(() {
       EasyLoading.dismiss(animation: true);
     });
@@ -41,19 +45,13 @@ class _ListScreenState extends State<ListScreen> {
       child: Column(
           children: <Widget>[
           Expanded(
-            
             child: ListView.separated(
                     shrinkWrap: true,
                     padding: const EdgeInsets.all(8.0),
                     separatorBuilder: (BuildContext context, int index) => const Divider(),
-                    itemCount: widget.covidData.length,
+                    itemCount: _covidDataForDisplay.length + 1,
                     itemBuilder: (BuildContext context, int index) {
-                      return ReusableCard(
-                          country: widget.covidData[index].country,
-                      new_cases: widget.covidData[index].new_cases,
-                      new_deaths: widget.covidData[index].new_deaths,
-                      activeCases: widget.covidData[index].activeCases,
-                      date: widget.covidData[index].date,);
+                      return index == 0 ? _searchBar() : _listItems(index - 1);
                     },
 
              ),
@@ -65,5 +63,40 @@ class _ListScreenState extends State<ListScreen> {
       ),
     );
 
+  }
+  _listItems(index) {
+    return ReusableCard(
+        country: _covidDataForDisplay[index].country,
+        new_cases: _covidDataForDisplay[index].new_cases,
+        new_deaths: _covidDataForDisplay[index].new_deaths,
+        activeCases: _covidDataForDisplay[index].activeCases,
+        date: _covidDataForDisplay[index].date);
+  }
+
+  _searchBar() {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          margin: EdgeInsets.only(top:10),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'country...',
+              fillColor: Colors.white60,
+              focusColor: Colors.white,
+              filled: true
+            ),
+            onChanged: (value) {
+              _covidDataForDisplay = widget.covidData; // This is added to bring back original values everytime we delete inputs as well
+              var inputCountry = value.toLowerCase();
+              setState(() {
+                _covidDataForDisplay = _covidDataForDisplay.where((item){
+                  var itemCountry = item.country.toLowerCase();
+                  return itemCountry.startsWith(inputCountry);
+                }).toList();
+              });
+            },
+          ),
+        ),
+      );
   }
 }
